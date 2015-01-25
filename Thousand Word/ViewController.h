76a773bbc6b1a5274cc6cdf421a2f0b29a57589a
delmarz <delmarz@delmarzs-Mac-Pro.local>
2015-01-25 18:49:11 +0800

    Initial Commit

---
+++ Thousand Word/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousand Word
+//
+//  Created by delmarz on 1/25/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
